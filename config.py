8fe1071396653006c105991cc38132804b700436
Fenny <126548831+fennytsai@users.noreply.github.com>
2026-07-23 08:11:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,36 +1,23 @@
 # =========================
 # 醫師設定
 # =========================
-
 DOCTORS = [
-
     {
         "name": "張哲彬",
-
         "hospital": "中國醫藥大學附設醫院",
-
         "dept": "中醫針灸",
-
         # 星期六
         "weekday": 5,
-
         "link":
         "https://www.cmuh.cmu.edu.tw/OnlineAppointment/DoctorInfo?flag=second&DocNo=24871&Docname=張哲彬"
     },
-
-
     {
         "name": "盧菁華",
-
         "hospital": "中國醫藥大學附設醫院",
-
         "dept": "神經內科",
-
         # 星期三
         "weekday": 2,
-
         "link":
         "https://www.cmuh.org.tw/OnlineAppointment/DoctorInfo?flag=second&DocNo=D7381&Docname=盧菁華"
     }
-
 ]
